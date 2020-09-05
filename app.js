a25ae14e4efe5a8ab8252b3c071a72e8b639a3cf
Mark <markburton88@live.com>
2020-09-06 00:32:58 +1000

    added twitter api code to project

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var twitterRouter = require('./routes/twitter');
+var bingNewsRouter = require('./routes/bingNews.js');
+var bingImagesRouter = require('./routes/bingImages.js');
 
 var app = express();
 
@@ -20,7 +22,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/trending?', twitterRouter);
+app.use('trending/news?', bingNewsRouter);
+app.use('trending/pics?', bingImagesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/twitter.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var router = express.Router();
+const twit = require('twit');
+
+const apiKey = 'JiZw0LDxyItpT9RowC0LjUUxU';
+const apiSecretKey = 'QOFXRDziLsysKNW6CZsP5huiVrUTO6WQKyji87chDkDy7A7VUJ';
+const accessToken = '1295958893827223557-97diUVsv3TgLcW304pR5CEBEkE1Cdq';
+const accessTokenSecret = 'RiSn6jXAi0WA9NHJCxmYM8bNnFYdQJLTPgGqnCFlERfnp';
+
+const T = new twit({
+  consumer_key: apiKey,
+  consumer_secret: apiSecretKey,
+  access_token: accessToken,
+  access_token_secret: accessTokenSecret
+})
+
+router.get('/', function(req, res, next) {
+  next(createError(404));
+  res.send('respond with a resource');
+});
+
+router.get('/:query', (req, res) => { 
+  let params = { query: req.params.query }
+  T.get('geo/search', params)
+
+  .then(result => {
+    let params = { 
+      lat: result.data.result.places[0].centroid[1], 
+      long: result.data.result.places[0].centroid[0] }
+      return T.get('trends/closest', params)
+
+  }) .then (result => {
+    let params = {
+        id: result.data[0].woeid,
+        exclude: 'hashtags' }
+    return T.get('trends/place', params)
+
+  }) .then (result => {
+    const top10Trends = []
+    for(let i = 0; i < 10; i++) {
+      top10Trends.push(result.data[0].trends[i].name)
+    }
+    return top10Trends;
+
+  }) .then (result => {
+    res.render("trending", { trending: result });
+    
+  }) .catch(function (err) {
+    console.log('caught error', err.stack)
+  })
+});
+
+module.exports = router;
