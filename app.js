e3a8a6cfaeac4469f7f6b6fe56a49aaaaabf9c1d
Mark <markburton88@live.com>
2020-09-05 21:46:36 +1000

    bare express-generator pug project

--- app.js
+++ app.js
@@ -1,32 +1,41 @@
-const express = require('express');
-const twitterRouter = require('./routes/twitter');
-const app = express();
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-app.get('/', (req, res) => {
-    const str =  '<!DOCTYPE html>' +
-    '<html>' +
-        '<head>' + 
-            '<style>' + 
-                'body { background-color: #847d95; }' +
-                'h1 { text-align: center; padding: 200px 0 0; font-family: verdana; font-weight: 100; font-size: 80px; color: #920e11; }' +
-                'h2 { font-family: helvetica; color: #920e11; }' +
-                'p { font-family: helvetica; }' +
-            '</style>' +         
-            '<title>Twitteliser</title>' + 
-        '</head>' +
-        '<body>' + 
-            '<h1>Twitteliser</h1>';;
+var app = express();
 
-    res.writeHead(200,{'content-type': 'text/html'});
-    res.write(str);
-    res.end();
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.use('/trending?', twitterRouter);
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-app.listen(port, function () {
-    console.log(`Express app listening at http://${hostname}:${port}/`);
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
+
+module.exports = app;
