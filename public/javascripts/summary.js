0eacae132ecb8d2ba4d7f9ed9c0fd4c0b0d6a873
Mark <markburton88@live.com>
2020-09-07 15:46:32 +1000

    Background and some formatting added

--- public/javascripts/summary.js
+++ public/javascripts/summary.js
@@ -1,14 +1,37 @@
-// Fetch the summary of the article from our Express API
+// Fetch and display news articles from Bing news API
 const fetchNews = (event) => { 
-  console.log(event); 
-  const trend = event.target.textContent.substring(1);
+  const trend = event.target.parentElement.firstChild.textContent.substring(1);
   fetch(`/trending/news/${trend}`)
   .then((res) => res.json())
   .then((data) => {
     let parent = event.target.parentElement;
     if (parent.childNodes.length <= 4) {
-      console.log(parent.childNodes.length);
       for (let story of data.stories) {      
+        let p = document.createElement("p");
+        let a = document.createElement("a");
+        a.href = story.url;
+        a.textContent = story.name
+        p.appendChild(a);
+        parent.appendChild(p);
+      }
+    } else {
+      for (let i = parent.childNodes.length-1; i > 3 ; i--)
+      parent.childNodes[i].remove();
+    }
+  })
+  .catch((error) => console.log(error));
+};
+
+// Fetch and display images from Bing images API
+const fetchPics = (event) => { 
+  console.log(event); 
+  const trend = event.target.parentElement.firstChild.textContent.substring(1);
+  fetch(`/trending/pics/${trend}`)
+  .then((res) => res.json())
+  .then((data) => {
+    let parent = event.target.parentElement;
+    if (parent.childNodes.length <= 4) {
+      for (let story of data.stories) {
         let p = document.createElement("p");
         let a = document.createElement("a");
         a.href = story.url;
@@ -17,29 +40,8 @@ const fetchNews = (event) => {
         parent.append(p);
       }
     } else {
-      console.log(parent.childNodes.length);
       for (let i = parent.childNodes.length-1; i > 3 ; i--)
       parent.childNodes[i].remove();
-    }
-  })
-  .catch((error) => console.log(error));
-};
-
-// Fetch the summary of the article from our Express API
-const fetchPics = (event) => { 
-  console.log(event); 
-  const trend = event.target.textContent.substring(1);
-  fetch(`/trending/news/${trend}`)
-  .then((res) => res.json())
-  .then((data) => {
-    let parent = event.target.parentElement;
-    for (let story of data.stories) {      
-      let p = document.createElement("p");
-      let a = document.createElement("a");
-      a.href = story.url;
-      a.textContent = story.name
-      p.appendChild(a);
-      parent.append(p);
     }
   })
   .catch((error) => console.log(error));
